065dca306bac86416031c31809afa0b5c4252833
ismailbaisa <ismailgesbaisa@gmail.com>
2021-01-07 03:31:00 +0700

    trying to fix dist

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,18 +2,28 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'imacakes',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: ''
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
 
   generate: {
-    dir: 'public'
+    dir: 'dist'
   },
 
   server: {
@@ -23,18 +33,17 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
-    "~assets/scss/style.scss"],
+    "~assets/scss/style.scss"
+  ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
@@ -47,10 +56,8 @@ export default {
     baseURL: 'https://my-json-server.typicode.com/ismailbaisa/evermos/',
   },
 
-  // Build Configuration (https://go.n  uxtjs.dev/config-build)
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-    publicPath: 'https://cdn.nuxtjs.org'
-  },
 
-  buildDir: 'nuxt-dist'
+  }
 }
